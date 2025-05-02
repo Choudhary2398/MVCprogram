0120c075fb5b3466ece0ed80cbff9d79a303d19c
Choudhary2398 <choudharyarif2398@gmail.com>
2025-05-02 17:31:43 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstMvcWebApps.MvcApplication" Language="C#" %>
---
+++ Logic.cs
@@ -0,0 +1,34 @@
+﻿using FirstMvcWebApps.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FirstMvcWebApps
+{
+    public class Logic
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbArif"].ToString());
+
+        public void AddEmployee(InsertEmployeeModel insertEmployee)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("[dbo].[InsertEmployee]", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@FirstName", insertEmployee.Empname);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+           
+        }
+    }
+}
