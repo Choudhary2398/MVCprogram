e6732dce9f6fe333e69b1ba715a88c60daf248eb
Choudhary2398 <choudharyarif2398@gmail.com>
2025-05-07 10:53:35 +0530

    Add files via upload

--- Logic.cs
+++ Logic.cs
@@ -1,34 +1,105 @@
-﻿using FirstMvcWebApps.Models;
+﻿using second_Mvc_Web_Apps.Models;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
-namespace FirstMvcWebApps
+namespace second_Mvc_Web_Apps
 {
     public class Logic
     {
-        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbArif"].ToString());
+        string conString = ConfigurationManager.ConnectionStrings["dbArif"].ConnectionString;
 
-        public void AddEmployee(InsertEmployeeModel insertEmployee)
+        public List<ListofEmployees> GetAll()
+        {
+            List<ListofEmployees> list = new List<ListofEmployees>();
+
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM employees", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    list.Add(new ListofEmployees
+                    {
+                        empid = Convert.ToInt32(rdr["empid"]),
+                        empname = rdr["empname"].ToString()
+                    });
+                }
+            }
+
+            return list;
+        }
+
+        public ListofEmployees GetByempid(int empid)
+        {
+            ListofEmployees list = new ListofEmployees();
+            ListofEmployees employees = null;
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                SqlCommand cmd = new SqlCommand("select * from employees where empid = @empid", con);
+                cmd.Parameters.AddWithValue("@empid", list.empid);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.Read())
+                {
+                    employees = new ListofEmployees
+                    {
+                        empid = Convert.ToInt32(rdr["id"]),
+                        empname = rdr["name"].ToString()
+                    };
+                }
+            }
+            return employees;
+        }
+
+        public void Insert(ListofEmployees employees)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                SqlCommand cmd = new SqlCommand("insert into employees (empname) values ( @empname)", con);
+
+                cmd.Parameters.AddWithValue("@empname", employees.empname);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(ListofEmployees employees)
         {
             try
             {
-                SqlCommand cmd = new SqlCommand("[dbo].[InsertEmployee]", con);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@FirstName", insertEmployee.Empname);
-                con.Open();
-                cmd.ExecuteNonQuery();
-                con.Close();
+                using (SqlConnection con = new SqlConnection(conString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE employees SET empname = @empname where empid = @empid", con);
+                    cmd.Parameters.AddWithValue("@empid", employees.empid);
+                    cmd.Parameters.AddWithValue("@empname", employees.empname);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
             }
             catch (Exception ex)
             {
-
+                Console.WriteLine(ex.Message);
             }
-           
+
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("delete from employees where empid = @empid", con);
+                cmd.Parameters.AddWithValue("@empid", id);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
         }
     }
 }
