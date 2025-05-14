4f8bc7156e6c97f9dac8676128e8c539426ad405
Choudhary2398 <choudharyarif2398@gmail.com>
2025-05-14 11:08:25 +0530

    Add files via upload

---
+++ EmailController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Emailtestprogram.Controllers
+{
+    public class EmailController: Controller
+    {
+        public ActionResult Index()
+        {
+            var emailService = new Email_logic();
+            string error;
+
+            if (emailService.SendEmail(out error))
+            {
+                ViewBag.Message = "✅ Email sent successfully!";
+            }
+            else
+            {
+                ViewBag.Message = "❌ Failed to send email: " + error;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Email_logic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+
+namespace Emailtestprogram
+{
+    public class Email_logic
+    {
+        public bool SendEmail(out string errorMessage)
+        {
+            try
+            {
+                var fromEmail = ConfigurationManager.AppSettings["EmailUsername"];
+                var fromPassword = ConfigurationManager.AppSettings["EmailPassword"];
+
+                var message = new MailMessage();
+                message.From = new MailAddress(fromEmail, "Arif Choudhary");
+                message.To.Add(new MailAddress("choudharyarif2398@gmail.com", "Recipient One"));
+                message.To.Add(new MailAddress("choudharyarif595@gmail.com", "Recipient Two"));
+                message.Subject = "Test Email from ASP.NET MVC";
+                message.Body = "Hello! This is a test email sent from an ASP.NET MVC app. By Arif";
+                message.IsBodyHtml = false;
+
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    Credentials = new NetworkCredential(fromEmail, fromPassword)
+                };
+
+                smtp.Send(message);
+                errorMessage = null;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return false;
+            }
+        }
+    }
+}
